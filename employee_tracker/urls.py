a300b511184874df63f25db729fb7ed153ba6024
argon08 <yunguta@gmail.com>
2015-05-03 22:27:47 +0000

    uploaded folders

---
+++ employee_tracker/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'employee_tracker.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^monitor/', 'monitor.views.monitor', name='monitor'),
+)
+
+urlpatterns += staticfiles_urlpatterns()
---
+++ monitor/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, render_to_response
+
+# Create your views here.
+
+def monitor(request):
+    return render_to_response('base.html', {})
